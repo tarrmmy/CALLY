ee9b5de5ef21d32068e9fcd356e0494aae986b20
tarrmmy <tarrmmy@gmail.com>
2024-09-25 09:51:04 +0100

    dashboard in progress

--- src/components/pages/main/Dashboard.js
+++ src/components/pages/main/Dashboard.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { Card } from 'antd';
+
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className='px-10'>
+      <div className='flex'>
+        <div className='w-3/4'>
+          <Card
+            style={{
+              width: 700,
+            }}
+          >
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+          </Card>
+        </div>
+        <div className='w-1/4'>
+          <Card
+            style={{
+              width: 300,
+            }}
+          >
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+          </Card>
+        </div>
+      </div>
+      <div>
+        <Card
+          style={{
+            width: 100,
+          }}
+        >
+          
+        </Card>
+      </div>
+    </div>
   )
 }
 
